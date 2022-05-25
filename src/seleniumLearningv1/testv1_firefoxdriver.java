3e3cd504e494a33f931e3fbd7b158929317e78d8
Rubén Romero Herrero <81327181+rubenromeroherrero@users.noreply.github.com>
2022-05-25 21:44:45 +0200

    webdrivers finished

--- src/seleniumLearningv1/testv1_firefoxdriver.java
+++ src/seleniumLearningv1/testv1_firefoxdriver.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class testv1_firefoxdriver {
     public static void main (String[] args){
         //Enlazar nuestra aplicación con un navegador web
+        //Enlazar el webdriver con nuestro archivo ejecutable para controlar nuestro navegador
         System.setProperty("webdriver.gecko.driver", "C:\\webrivers\\geckodriver.exe");
 
         //Creamos variable de tipo WebDriver para conectar con el navegador firefox
